e0edebe1362689fe8bb9490ca7fd2f5547be0261
Natasha Moorfield <mailbox@natashamoorfield.co.uk>
2020-06-14 16:04:06 +0100

    Add EntityName Class
    Entities have names but we may want to do more with those names than simply
    store them or display them as is. [1384]

---
+++ src/entity_name.py
@@ -0,0 +1,17 @@
+class EntityName(object):
+    """
+    Entities have names but
+    we may want to do more with those names than simply store them or display them as is.
+    """
+    def __init__(self, name):
+        self.name = name
+
+    def display_name(self):
+        return self.name
+
+    def index_name(self):
+        return self.name
+
+
+class PlaceName(EntityName):
+    pass
