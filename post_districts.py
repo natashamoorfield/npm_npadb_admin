f177ea043102f7f785a537a63c1018cbe8390d60
Natasha Moorfield <mailbox@natashamoorfield.co.uk>
2020-06-15 16:43:57 +0100

    Solve the Stockton Postcode Problem
    [1417]

--- post_districts.py
+++ post_districts.py
@@ -3,6 +3,23 @@ import csv
 import os
 
 
+class PostCodeToDistrictConverter(object):
+    stockton_division = {
+        'DL2': 2610,
+        'TS2': 2610,
+        'TS8': 611,
+        'TS15': 611,
+        'TS16': 2610,
+        'TS17': 611,
+        'TS18': 2610,
+        'TS19': 2610,
+        'TS20': 2610,
+        'TS21': 2610,
+        'TS22': 2610,
+        'TS23': 2610
+    }
+
+
 class MyEnvironment(object):
     def __init__(self):
         self.dbc = mysql.connector.connect(
